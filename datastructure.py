30c5b73d3f9c6f086df6afdba9c986f4e2ac1ad0
Haisam Ido <haisam.ido@honeywell.com>
2014-06-10 17:16:02 -0400

    First attempt at python (incomplete)

--- datastructure.py
+++ datastructure.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 import sys
 import re
 
+type = "";
+hierarchy_pc = dict()
+hierarchy    = dict()
+
+# Read data from STDIN
+#  STDIN must be comma separated data with the following columns:
+#  userid,parentid,childid,childname,idirectory
+
+
+#-------------------------------------------------------------------------------
+# Step 1 of 3
+#
+# Read lines from STDIN
+#  STDIN must be comma separated data with the following columns:
+#  userid,parentid,childid,childname,idirectory
+#
+#-------------------------------------------------------------------------------
 for line in sys.stdin:
   
     if re.match(r'^\s*$|^\s*#', line):
-      continue
+        continue
       
     print line,
 
@@ -18,4 +36,11 @@ for line in sys.stdin:
     childName   = line_split[3]
     isDirectory = line_split[4]
         
+    if isDirectory == 1:
+        type = 'folder'
+
+    if isDirectory == 0:
+        type = 'file'
+
+    hierarchy[parentId]['children'][childId] = { 'name': childName }
     
